39fe1859d6854beda85a44b8b8c8fcbf34910a2a
yugo-ibuki <y.ibuki91@gmail.com>
2022-12-27 20:59:49 +0900

    クリックボタンを追加

--- Fyne/main.go
+++ Fyne/main.go
@@ -4,16 +4,27 @@ import (
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/container"
 	"fyne.io/fyne/widget"
+	"strconv"
 )
 
 func main() {
+	c := 0
 	a := app.New()
-
+	l := widget.NewLabel("Hello Fyne!")
 	w := a.NewWindow("Hello")
+	//w.SetContent(
+	//	container.NewVBox(
+	//		widget.NewLabel("Hello Fyne!"),
+	//		widget.NewLabel("This is sampleapplication!"),
+	//	),
+	//)
 	w.SetContent(
 		container.NewVBox(
-			widget.NewLabel("Hello Fyne!"),
-			widget.NewLabel("This is sampleapplication!"),
+			l,
+			widget.NewButton("Click Me!", func() {
+				c++
+				l.SetText("Hello Fyne! " + strconv.Itoa(c))
+			}),
 		),
 	)
 
